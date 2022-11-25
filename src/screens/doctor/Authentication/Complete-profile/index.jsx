318c7b94216fde928e20f37fbe3944e65d798b34
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-11-25 20:12:25 +0500

    Added old code to new repo

--- src/screens/doctor/Authentication/Complete-profile/index.jsx
+++ src/screens/doctor/Authentication/Complete-profile/index.jsx
@@ -11,4 +11,4 @@ const CompleteProfile = () => {
   );
 };
 
-export default CompleteProfile;
+export default Complete - Profile;
--- src/screens/doctor/Authentication/Complete-profile/styles.js
+++ src/screens/doctor/Authentication/Complete-profile/styles.js
@@ -1,11 +1,32 @@
 import {StyleSheet} from 'react-native';
 
-import colors from '../../../../utils/styles/themes/colors';
-import fonts from '../../../../utils/styles/themes/fonts';
-import dimensions from '../../../../utils/styles/themes/dimensions';
+import colors from '../../../utils/styles/themes/colors';
+import fonts from '../../../utils/styles/themes/fonts';
+import dimensions from '../../../utils/styles/themes/dimensions';
 
 export const styles = StyleSheet.create({
   root: {},
 
-  container: {},
+  scrollContainer: {
+    width: '100%',
+  },
+
+  child: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+
+  formContainer: {
+    width: '100%',
+    flex: 1,
+  },
+  text: {
+    fontSize: fonts.size.font16,
+    fontWeight: fonts.weight.normal,
+  },
+
+  buttonContainer: {
+    width: '100%',
+  },
 });
