43e713b870e41061eeebaba041924d6ca0321669
Ali Hamza <hamza120025@gmail.com>
2022-11-26 16:35:01 +0500

    fixed dimensions.js

--- src/utils/styles/themes/dimensions.js
+++ src/utils/styles/themes/dimensions.js
@@ -1,13 +1,8 @@
 import metrics from './metrics';
 
 const dimensions = {
-<<<<<<< HEAD
   Height: metrics.screenHeight,
   Width: metrics.screenWidth,
-=======
-  Width: metrics.screenWidth,
-  Height: metrics.screenHeight,
->>>>>>> 93ce3092bba4037ab0e9dfab88f6ff9628f63477
 };
 
 export default dimensions;
