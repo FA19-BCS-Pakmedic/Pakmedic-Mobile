7830da3226ab425c1c04485567ca328c2ad9a8a0
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-11-26 15:46:26 +0500

    New onboarding screen designed for Doctor flow

--- App.js
+++ App.js
@@ -7,16 +7,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import RootNavigation from './src/setup/navigation/root.navigation';
 import CompleteProfile from './src/screens/doctor/Authentication/Complete-profile';
 import Register from './src/screens/doctor/Authentication/Register';
-import Login from './src/screens/shared/Authentication/Login';
+import OnBoarding from './src/screens/doctor/OnBoarding';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigation />
-    </NavigationContainer>
-    // <SafeAreaView style={styles.root}>
-    //   <CompleteProfile />
-    // </SafeAreaView>
+    // <NavigationContainer>
+    //   <RootNavigation />
+    // </NavigationContainer>
+    <SafeAreaView style={styles.root}>
+      <OnBoarding />
+    </SafeAreaView>
   );
 };
 
---
+++ src/screens/doctor/OnBoarding/styles.js
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+
+import colors from '../../../utils/styles/themes/colors';
+import fonts from '../../../utils/styles/themes/fonts';
+import dimensions from '../../../utils/styles/themes/dimensions';
+
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    height: dimensions.Height,
+    width: dimensions.Width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  logo: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  heading: {
+    width: dimensions.Width / 1.3,
+    fontSize: fonts.size.font20,
+    fontWeight: fonts.weight.bold,
+    textAlign: 'center',
+  },
+
+  overlay: {
+    padding: 20,
+    height: dimensions.Height / 2.4,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+});
