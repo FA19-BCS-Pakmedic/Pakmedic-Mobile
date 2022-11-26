6b37f4a6ae62911278dc290ec1a24225653b189e
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-11-26 20:40:48 +0500

    integrating changes for development branch

--- src/screens/doctor/On-Boarding/styles.js
+++ src/screens/doctor/On-Boarding/styles.js
@@ -5,7 +5,34 @@ import fonts from '../../../utils/styles/themes/fonts';
 import dimensions from '../../../utils/styles/themes/dimensions';
 
 export const styles = StyleSheet.create({
-  root: {},
+  root: {
+    flex: 1,
+    height: dimensions.Height,
+    width: dimensions.Width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-  container: {},
+  logo: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  heading: {
+    width: dimensions.Width / 1.4,
+    fontSize: fonts.size.font20,
+    fontWeight: fonts.weight.bold,
+    textAlign: 'center',
+  },
+
+  overlay: {
+    padding: 20,
+    height: dimensions.Height / 2.4,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderTopLeftRadius: 60,
+    borderTopRightRadius: 60,
+  },
 });
