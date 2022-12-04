11cb31ec06970818eede25297e065f62c55c7206
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-12-04 21:55:53 +0500

    Added Community Detail boiler plate code and fixed Button comp OnPress prop

--- src/screens/shared/Support-communities/Community-details/index.jsx
+++ src/screens/shared/Support-communities/Community-details/index.jsx
@@ -1,13 +1,49 @@
 import {View, Text} from 'react-native';
-import React from 'react';
-
+import React, {useState} from 'react';
+import StaticContainer from '../../../../containers/StaticContainer';
+import DropDownPicker from 'react-native-dropdown-picker';
+DropDownPicker.setListMode('SCROLLVIEW');
 import {styles} from './styles';
+import dimensions from '../../../../utils/styles/themes/dimensions';
+import Search from '../../../../components/shared/CommunitySearch';
 
-const CommunityDetails = () => {
+const CommunityDetails = ({route}) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'C/Dermatologist', value: 'home'},
+    {label: 'C/Dermatologist', value: 'haris'},
+    {label: 'C/Dermatologist', value: 'moeed'},
+    {label: 'C/Dermatologist', value: 'ali'},
+  ]);
+
+  const {communityName} = route.params;
   return (
-    <View>
-      <Text>CommunityDetails</Text>
-    </View>
+    <StaticContainer customHeaderEnable={true} customHeaderName={communityName}>
+      <View style={styles.container}>
+        <View style={styles.search}>
+          <View>
+            <DropDownPicker
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              setItems={setItems}
+              style={styles.dropDown}
+              dropDownContainerStyle={styles.dropDownContainer}
+              placeholder="Home"
+              textStyle={{
+                fontSize: 12,
+              }}
+              maxHeight={dimensions.Height * 0.15}
+            />
+          </View>
+          <Search />
+        </View>
+        <Button />
+      </View>
+    </StaticContainer>
   );
 };
 
