672c7fb16146bea19f04d74f6c43bccdd20ba284
Ali Hamza <hamza120025@gmail.com>
2023-03-17 21:10:45 +0500

    changed apiEndpoint file

--- src/utils/constants/APIendpoint.js
+++ src/utils/constants/APIendpoint.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-export const apiEndpoint = 'http://192.168.100.14:8000/api/v1/';
-=======
 import {BASE_ADDRESS_DEV, BASE_URL, PORT} from '@env';
 
 export const apiEndpoint = BASE_ADDRESS_DEV; //TODO: Replace this with link fetched from env vairable
 export const baseUrl = `${BASE_URL}:${PORT}`;
->>>>>>> c386ab18c7f9cec50f1d0fe11f61e8c1d8a73257
